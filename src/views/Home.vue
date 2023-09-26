d740705999e7f32cf21ef496a0322ddd939d22a3
Lertdg <tplermit@gmail.com>
2023-09-26 14:02:49 -0400

    decode

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,13 +5,17 @@
 
 <script setup>
 // import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from "vue";
 import { StreamBarcodeReader } from "vue-barcode-reader";
 
+const code = ref(null);
 
 const onDecode = (text) => {
+  code.value = text;
   console.log(`Decode text from QR code is ${text}`)
 }
 const onLoaded = () => {
+  
   console.log(`Ready to start scanning barcodes`)
 }
 
